d873d386371194a354001abb5522b375003a24d9
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2020-03-24 21:28:12 +0100

    Add API to change the “life” status of a cell.

--- src/gol.js
+++ src/gol.js
@@ -1,9 +1,27 @@
 const grid = (width, height) => {
 	return Array(height)
 		.fill(null)
-		.map(() => Array(width).fill(false, 0, width));
+		.map(() => {
+			return Array(width).fill(false, 0, width);
+		});
 };
+
+const spawn = (grid, x, y) => {
+	return _cellValue(grid, x - 1, y - 1, true);
+}
+
+const kill = (grid, x, y) => {
+	return _cellValue(grid, x - 1, y - 1, false);
+}
+
+const _cellValue = (grid, x, y, value) => {
+	let copy = Array.from(grid);
+	copy[x][y] = value;
+	return copy;
+}
 
 module.exports = {
 	grid: grid,
+	spawn: spawn,
+	kill: kill,
 };
--- test/gol.test.js
+++ test/gol.test.js
@@ -11,3 +11,32 @@ describe('grid', () => {
 		]);
 	});
 });
+
+describe('spawn', () => {
+	test('spawns a new cell on a given coordinate on a grid', () => {
+		let grid = gol.grid(5, 5);
+		expect(gol.spawn(grid, 3, 3)).toEqual([
+			[false, false, false, false, false],
+			[false, false, false, false, false],
+			[false, false, true , false, false],
+			[false, false, false, false, false],
+			[false, false, false, false, false],
+		]);
+	});
+});
+
+
+describe('kill', () => {
+	let grid = gol.spawn(gol.grid(5, 5), 3, 3);
+	grid = gol.spawn(grid, 4, 3);
+
+	test('kills a cell on a given coordinate on a grid', () => {
+		expect(gol.kill(grid, 3, 3)).toEqual([
+			[false, false, false, false, false],
+			[false, false, false, false, false],
+			[false, false, false, false, false],
+			[false, false, true , false, false],
+			[false, false, false, false, false],
+		]);
+	});
+});
